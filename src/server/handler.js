dbb46fee3463ed763ffbb4882bf4ba37455d246a
Kahlil <sammybangun@gmail.com>
2024-11-15 18:19:23 +0700

    Integrasi ke Cloud Firestore

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,9 +1,10 @@
 const predictClassification = require("../services/inferenceService");
 const crypto = require("crypto");
+const storeData = require("../services/storeData");
 
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
-  const { model } = request.app.model;
+  const { model } = request.server.app;
   const { confidenceScore, label, explanation, suggestion } =
     await predictClassification(model, image);
 
@@ -18,6 +19,8 @@ async function postPredictHandler(request, h) {
     confidenceScore: confidenceScore,
     createdAt: createdAt,
   };
+
+  await storeData(id, data);
 
   const response = h.response({
     status: "success",
